ed95a14906578d5087fc5dc2e94568af201d2ece
Simon Gravelle <simon.gravelle@live.fr>
2024-08-20 19:34:59 +0200

    updated test

--- automated-testing/generated-codes/chapter2/test_Prepare.py
+++ automated-testing/generated-codes/chapter2/test_Prepare.py
@@ -16,3 +16,21 @@ print(self.array_epsilon_ij)
 print("array_sigma_ij:")
 print(self.array_sigma_ij)
 
+
+
+from Prepare import Prepare
+
+self = Prepare(number_atoms=[2, 3],
+    epsilon=[0.1, 1.0], # kcal/mol
+    sigma=[3, 6], # A
+    atom_mass=[1, 1], # g/mol
+    )
+print("Reference energy:")
+print(self.reference_energy)
+print("Reference distance:")
+print(self.reference_distance)
+print("array_epsilon_ij:")
+print(self.array_epsilon_ij)
+print("array_sigma_ij:")
+print(self.array_sigma_ij)
+
