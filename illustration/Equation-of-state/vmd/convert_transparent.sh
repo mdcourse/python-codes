73022b0e53b91937eff26f0fd48fd979336a2478
Simon Gravelle <simon.gravelle@live.fr>
2024-08-26 20:54:56 +0200

    added vmd status

---
+++ illustration/Equation-of-state/vmd/convert_transparent.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export LC_NUMERIC="en_US.UTF-8"
+set -e
+
+# 1) Generate white background movie
+for file in _light.*.ppm; 
+do 
+	convert $file -transparent white ${file:0:12}.png;
+done
+img2webp -o avatar.webp -q 30 -mixed -d 25 _light*.png
+
+# 2) Generate black background movie
+for file in _dark.*.ppm; 
+do 
+	convert $file -transparent black ${file:0:11}.png;
+done
+img2webp -o avatar-dm.webp -q 30 -mixed -d 25 _dark*.png
