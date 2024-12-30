a87f0da4f27b2c5b294150a01dd13a07fc0d64d5
Simon Gravelle <simon.gravelle@live.fr>
2024-12-30 16:32:39 +0100

    made potential numba compatible

--- python-codes/potentials.py
+++ python-codes/potentials.py
@@ -1,8 +1,8 @@
+from numba import njit
 
-
+@njit
 def potentials(epsilon, sigma, r, derivative=False):
     if derivative:
         return 48 * epsilon * ((sigma / r) ** 12 - 0.5 * (sigma / r) ** 6) / r
     else:
         return 4 * epsilon * ((sigma / r) ** 12 - (sigma / r) ** 6)
-
